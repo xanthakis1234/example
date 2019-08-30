55337e50cef65b66f0d3cd842ec7195dd3471426
Nektarios Xanthakis <Nektarios.Xanthakis@eurodyn.com>
2019-08-30 17:36:33 +0300

    initial commit

---
+++ todo/src/test/java/com/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
