98e1727fabb3b11966922b90d5f19b0177167f28
nxan <nxan@xanthakis.eurodyn.com>
2019-09-09 14:23:26 +0300

    Update functionality implemented

--- todo/src/main/java/com/todo/controllers/TodoRestController.java
+++ todo/src/main/java/com/todo/controllers/TodoRestController.java
@@ -13,52 +13,46 @@ import org.springframework.web.bind.annotation.RestController;
 import com.todo.model.Task;
 import com.todo.services.TodoService;
 
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TodoRestController {
-	
+
 	@Autowired
 	private TodoService service;
-	
-	@GetMapping(path="/todo/hello")
-	public String hello () {
+
+	@GetMapping(path = "/todo/hello")
+	public String hello() {
 		return "ALOXA";
 	}
 
-/*	@GetMapping(path="todo/getTasks")
-	public List<Task> getTasks() {
-		return service.getTasks();
-	}*/
-	
-	@GetMapping(path="todo/getTasks")
+	@GetMapping(path = "todo/getTasks")
 	public List<Task> getAllTasks() {
 		return service.getAll();
-
-	}	
-	
-	@PostMapping(path="todo/createTask")
-	public void createTask(@RequestBody Task task) {
-		
-		 service.createTask(task);
 	}
-	
-	@GetMapping(path="todo/{id}")
+
+	@PostMapping(path = "todo/createTask")
+	public void createTask(@RequestBody Task task) {
+
+		service.createTask(task);
+	}
+
+	@GetMapping(path = "todo/{id}")
 	public Task getTask(@PathVariable int id) {
 		return service.getTaskFromId(id);
 	}
-	
-	@PutMapping(path="todo/updateTask/{id}")
-	public Task updateTask(@PathVariable int id, @RequestBody Task task) {		
+
+	@PutMapping(path = "todo/updateTask/{id}")
+	public Task updateTask(@PathVariable int id, @RequestBody Task task) {
 		Task updatedTask = service.updateTask(task);
 		return updatedTask;
 	}
-	
+
 	@DeleteMapping("todo/deleteTask/{id}")
 	public void deleteTask(@PathVariable int id) {
 		service.deleteTask(id);
-	}	
-	
+	}
+
 	public String get() {
-	    return "Hello JUnit 5";
-	}		
+		return "Hello JUnit 5";
+	}
 }
--- todo/src/main/java/com/todo/services/TodoService.java
+++ todo/src/main/java/com/todo/services/TodoService.java
@@ -1,12 +1,9 @@
 package com.todo.services;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.todo.model.Task;
 import com.todo.repository.TaskRepository;
@@ -16,17 +13,9 @@ public class TodoService {
 	
 	@Autowired
 	private TaskRepository taskRepository;
-	
-
 	private static List<Task> listOfTasks = new ArrayList();
-	private static int id = 0;
-	
-	static {
-		listOfTasks.add(new Task(++id, "Learn Guitar", new Date(), false));
-		listOfTasks.add(new Task(++id,  "Learn Angular", new Date(), false));
-		listOfTasks.add(new Task(++id,  "Learn Spring", new Date(), false));
-	}
 
+	
 	public List<Task> getAll() {
 		return taskRepository.findAll();
 	}	
@@ -42,10 +31,9 @@ public class TodoService {
 	public void deleteTask(int id) {
 		taskRepository.deleteById(id);
 	}
-	
 	public Task updateTask(Task task) {
-		deleteTask(task.getId());
-		listOfTasks.add(task);
-		return task;
-	} 
+		taskRepository.save(task);
+		Task updatedTask = task;
+		return updatedTask;
+	}
 }
