b2ef3f7c8b3f4636fe2e123dfcf9c29d1aa3059c
Nektarios Xanthakis <Nektarios.Xanthakis@eurodyn.com>
2019-09-16 13:15:50 +0300

    Fixed Translate on Modal

--- angularTodo/Todofrontend/src/app/app.component.ts
+++ angularTodo/Todofrontend/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import {  OnInit } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import { Constants } from './constans';
