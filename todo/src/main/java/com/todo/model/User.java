0551eb3473226d45d86e7997d0b62a1bd9e8e04c
nxan <nxan@xanthakis.eurodyn.com>
2019-10-11 12:14:42 +0300

    Include firstname, lastname and email to User model

--- todo/src/main/java/com/todo/model/User.java
+++ todo/src/main/java/com/todo/model/User.java
@@ -29,13 +29,25 @@ public class User {
 	@Column(name="password")
 	private String password;
 	
+	@Column(name="firstname")
+	private String firstname;
+	
+	@Column(name="lastname")
+	private String lastname;
+	
+	@Column(name="email")
+	private String email;
+	
 	@OneToMany(fetch=FetchType.LAZY,
 			mappedBy="user", cascade=CascadeType.ALL)
 	private List<Task> tasks;
 
-	public User(String username, String password) {
+	public User(String username, String password, String firstname, String lastname, String email) {
 		this.username = username;
 		this.password = password;
+		this.password = firstname;
+		this.password = lastname;
+		this.password = email;
 	}
 
 	public User() {
@@ -61,6 +73,30 @@ public class User {
 		return password;
 	}
 
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -76,8 +112,8 @@ public class User {
 
 	@Override
 	public String toString() {
-		return "User [iduser=" + iduser + ", username=" + username + ", password=" + password + ", tasks=" + tasks
-				+ "]";
+		return "User [iduser=" + iduser + ", username=" + username + ", password=" + password + ", firstname="
+				+ firstname + ", lastname=" + lastname + ", email=" + email + ", tasks=" + tasks + "]";
 	}
 	
 }
