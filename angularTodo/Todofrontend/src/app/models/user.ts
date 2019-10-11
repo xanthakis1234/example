3b583ee3eb21a5cbb011157f10458df2863ec52e
Nektarios Xanthakis <Nektarios.Xanthakis@eurodyn.com>
2019-10-11 12:13:45 +0300

    Include firstname, lastname and email fields on User model

--- angularTodo/Todofrontend/src/app/models/user.ts
+++ angularTodo/Todofrontend/src/app/models/user.ts
@@ -2,8 +2,8 @@ export class User {
     id: number;
     username: string;
     password: string;
-    // firstName: string;
-    // lastName: string;
-    // email: string;
+    firstName: string;
+    lastName: string;
+    email: string;
     // token?: string;
 }
