35e2d2a1ebc90d840f3444a9118e9a445c09c49a
Nektarios Xanthakis <Nektarios.Xanthakis@eurodyn.com>
2019-10-14 17:36:53 +0300

    Jwt on Login Implemented

--- angularTodo/Todofrontend/src/app/service/authentication-service.service.ts
+++ angularTodo/Todofrontend/src/app/service/authentication-service.service.ts
@@ -11,40 +11,40 @@ import { User } from '../models/user';
 export class AuthenticationService {
   private currentUserSubject:BehaviorSubject<User>;
   public currentUser: Observable<User>;
+  private responseBody = null;
 
   constructor(private http: HttpClient) {
-    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    //this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(this.responseBody));
     this.currentUser = this.currentUserSubject.asObservable();
    }
 
    readonly APP_URL = 'http://localhost:8080/todo';
 
-   public get currentUserValue(): User {
-     return this.currentUserSubject.value;
-   }
+    public get currentUserValue(): User {
+      return this.currentUserSubject.value;
+    }
 
    login(username: string, password: string){
-    console.log(this.isUserLoggedIn())
-     return this.http.post<any>(this.APP_URL + '/login', { username, password })
-      .pipe(map(user=>{
-        //if (user && user.token){
-        if (user){
-          localStorage.setItem('currentUser', JSON.stringify(user));
-          this.currentUserSubject.next(user);
-          console.log(this.isUserLoggedIn())
+        return this.http.post<any>(this.APP_URL + '/authenticate', { username, password }, 
+        { observe: 'response' })
+      .pipe(map(response=>{
+        this.responseBody = response.body;
+        if (response.headers.get('Token')){
+           localStorage.setItem('currentUser', response.headers.get('Token'));
+          this.currentUserSubject.next(response.body);
           console.log("login done");
-         //console.log(this.currentUserSubject.value)
+          console.log(this.currentUserSubject.value)
         }
         console.log("login not done");
-        return user;
+        return response;
       }));
      
    }
 
    isUserLoggedIn(){
-    let user = localStorage.getItem('currentUser');
-    //console.log("check local storage", localStorage.getItem('currentUser'))
-    if (user == null){
+    let token = localStorage.getItem('currentUser');
+    if (token == null){
       return false;
     } else {
       return true;
